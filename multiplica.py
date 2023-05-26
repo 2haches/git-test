3b9da0315add7ee86fed23143aee63c706717f50
“3haches” <3haches@hotmail.com>
2023-05-27 01:07:02 +0200

    tabla multiplicar x 5

--- multiplica.py
+++ multiplica.py
@@ -1,3 +1,7 @@
-# Programa que suma 2 números
-suma = 1+1
-print(“Suma = “, suma)
+# Programa multiplica.py
+factor = 5 # haremos la tabla del 5
+desde = 1 #límite inferior (incluido)
+hasta = 10 #límite superior (incluido también)
+
+for f in range(desde, hasta + 1):
+    print(f'{factor} x {f} =  {factor * f}')
